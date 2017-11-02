fixed underflow with log probs

LL_LVM.py
@@ -18,8 +18,8 @@ class LL_LVM:
         #self.sigma_x = ln.inv(self.sigma_x_inv)
         
         #add some jitter to condition properly
-        jitter = np.random.chisquare(.5,self.Dy*self.N)
-        self.sigma_x_inv = self.sigma_x_inv + np.diag(jitter)
+       # jitter = np.random.chisquare(.5,self.Dy*self.N)
+        #self.sigma_x_inv = self.sigma_x_inv + np.diag(jitter)
         self.sigma_x = chol_inv(self.sigma_x_inv)
         
         self.t_priorcov = ln.inv(alpha*np.identity(self.N) + 2*self.L)
@@ -57,28 +57,32 @@ class LL_LVM:
         #proposed contains all latent variables in one array
         if proposed:
             #calculate likelihood under proposed value
-            Cfactor = np.log(matrix_normal(self.Cprop,np.zeros(shape=(self.Dy, self.N*self.Dt)),np.identity(self.Dy),self.C_priorcov))
-            tfactor = np.log(matrix_normal(self.tprop,np.zeros(self.Dt),np.identity(self.Dt),self.t_priorcov))
+            #Cfactor = np.log(matrix_normal(self.Cprop,np.zeros(shape=(self.Dy, self.N*self.Dt)),np.identity(self.Dy),self.C_priorcov))
+            #tfactor = np.log(matrix_normal(self.tprop,np.zeros(self.Dt),np.identity(self.Dt),self.t_priorcov))
+            Cfactor = matrix_normal_log(self.Cprop,np.zeros(shape=(self.Dy, self.N*self.Dt)),np.identity(self.Dy),self.C_priorcov)
+            tfactor = matrix_normal_log(self.tprop,np.zeros(self.Dt),np.identity(self.Dt),self.t_priorcov)
             mu_x = np.matrix(self.sigma_x) * self.eprop.reshape((self.N*self.Dy,1))
-            xfactor = np.log(scipy.stats.multivariate_normal.pdf(self.x.reshape((self.N*self.Dy,1)).T,mean= list(mu_x.flat),cov=self.sigma_x))
-            print Cfactor, tfactor, xfactor
+            xfactor = scipy.stats.multivariate_normal.logpdf(self.x.reshape((self.N*self.Dy,1)).T,mean= list(mu_x.flat),cov=self.sigma_x)
+            #print Cfactor, tfactor, xfactor
             return Cfactor + tfactor + xfactor
             
         else:
             #if proposed is false just calculate it under the current variables
-            Cfactor = np.log(matrix_normal(self.C,np.zeros(shape=(self.Dy, self.N*self.Dt)),np.identity(self.Dy),self.C_priorcov))
-            tfactor = np.log(matrix_normal(self.t,np.zeros(self.Dt),np.identity(self.Dt),self.t_priorcov))
+            #Cfactor = np.log(matrix_normal(self.C,np.zeros(shape=(self.Dy, self.N*self.Dt)),np.identity(self.Dy),self.C_priorcov))
+            #tfactor = np.log(matrix_normal(self.t,np.zeros(self.Dt),np.identity(self.Dt),self.t_priorcov))
+            Cfactor = matrix_normal_log(self.C,np.zeros(shape=(self.Dy, self.N*self.Dt)),np.identity(self.Dy),self.C_priorcov)
+            tfactor = matrix_normal_log(self.t,np.zeros(self.Dt),np.identity(self.Dt),self.t_priorcov)
             mu_x = np.matrix(self.sigma_x) * self.e.reshape((self.N*self.Dy,1))
-            xfactor = np.log(scipy.stats.multivariate_normal.pdf(self.x.reshape((self.N*self.Dy,1)).T,mean= list(mu_x.flat),cov=self.sigma_x))
-            print Cfactor, tfactor, xfactor
+            xfactor = scipy.stats.multivariate_normal.logpdf(self.x.reshape((self.N*self.Dy,1)).T,mean= list(mu_x.flat),cov=self.sigma_x)
+            #print Cfactor, tfactor, xfactor
             return Cfactor + tfactor + xfactor
     
     #update with Metropolis-Hastings step
-    def update(self,proposed):
+    def update(self):
         #calculate likelihoods
         Lprime = self.likelihood(proposed=True)
         L = self.likelihood()
-        
+        print Lprime - L
         #calculate acceptance probability
         a = min(1.0,np.exp(Lprime - L))
         acceptance = np.random.choice([0,1],p=(1-a,a))
@@ -110,6 +114,7 @@ class LL_LVM:
         self.update()
         #store new likelihood
         self.likelihoods.append(self.likelihood())
+
 
 
 

swissroll_example.py
@@ -5,31 +5,39 @@ t_true = data[1].T
 Dy,N = x.shape
 Dt = 1
 
-tinit = np.random.multivariate_normal([0] * Dt * N, np.identity(Dt * N)*5).reshape((1,N))
-Cinit = np.random.multivariate_normal([0] * Dt * N * Dy, np.identity(Dt * N * Dy)*5).reshape(Dy,Dt*N)
+tinit = np.random.multivariate_normal([0] * Dt * N, np.identity(Dt * N)*.25).reshape((1,N))
+Cinit = np.random.multivariate_normal([0] * Dt * N * Dy, np.identity(Dt * N * Dy)*.25).reshape(Dy,Dt*N)
 
 #build nearest neighbor graph
-G = neighbors.kneighbors_graph(x.T,3).toarray()
+G = neighbors.kneighbors_graph(x.T,7).toarray()
 G = ((G + G.T) > 0) * 1.0
 
-#to initialize from the prior on t
+#set user-defined parameters
+alpha = 1.0
+gamma =100.0
+epsilon = .00001
+#V = np.identity(Dy) * gamma
+V = np.cov(x) * gamma
+
+#to initialize from the priors
 #degree = np.sum(G,1)
 #L = np.diag(degree) - G
 #omega_inv = np.kron(2*L, np.identity(Dt))
 #Pi_inv = chol_inv(alpha * np.identity(N*Dt) + omega_inv)
+#J = np.kron(np.ones(shape=(N,1)),np.identity(Dt))
+#C_priorcov = ln.inv(epsilon * J * J.T + omega_inv)
 #tinit = np.random.multivariate_normal(np.zeros(Dt*N),Pi_inv).reshape((Dt,N))
+#Cinit = np.random.multivariate_normal(np.zeros(shape=(Dy*N*Dt)),np.kron(np.identity(Dy),C_priorcov)).reshape((Dy,Dt*N))
 
-#set user-defined parameters
-alpha = 1.0
-gamma =20.0
-epsilon = .01
-V = np.identity(Dy) * gamma
-#V = np.cov(x)
 
-model = LL_LVM(G,epsilon,alpha,V,Cinit,tinit,x,5.0)
+model = LL_LVM(G,epsilon,alpha,V,Cinit,tinit,x,.5)
 model.likelihood()
 model.propose()
 model.likelihood(proposed=True)
+
+for i in range(10000):
+    model.MH_step()
+
 
 
 