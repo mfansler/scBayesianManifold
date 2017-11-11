ff62ef6eb0010a1621a7ebddc4ec87afd0f7f03f
Merv Fansler <mmfansler@gmail.com>
2017-11-11 16:10:12 -0500

    DRYer; likelihood cache

--- lllvm/LL_LVM.py
+++ lllvm/LL_LVM.py
@@ -44,8 +44,8 @@ class LL_LVM:
         self.t_priorprc = self.alpha*eye(self.N) + 2*self.L
         self.C_priorprc = epsilon*self.J.dot(self.J.T) + self.omega_inv
 
-        self.Cpropcov = np.identity(self.N * self.Dy * self.Dt) * stepsize
-        self.tpropcov = np.identity(self.N * self.Dt) * stepsize
+        self.Cpropcov = eye(self.N * self.Dy * self.Dt) * stepsize
+        self.tpropcov = eye(self.N * self.Dt) * stepsize
         
         # create a dictionary of neighbors
         self.neighbors = G.tolil().rows
@@ -55,94 +55,88 @@ class LL_LVM:
         self.C, self.t, self.x = Cinit, tinit, xinit
 
         # list of Dy by Dt numpy arrays for each observation's linear map C_i
-        self.Ci = [self.C[:,np.arange(i*self.Dt,(i+1)*self.Dt)] for i in range(self.N)]
-        if yobserved != 0: #for the noisy LL_LVM model
+        if yobserved != 0:  # for the noisy LL_LVM model
             self.y = yobserved
         
-        #self.e = [-1 * np.sum([self.Vinv * (self.Ci[i] + self.Ci[j])*(self.t[:,i] - self.t[:,j]) for j in self.neighbors[i]]) for i in range(self.N)]
-        self.e = np.array([-1 * np.array([self.Vinv * np.matrix((self.Ci[i] + self.Ci[j]))*np.matrix((self.t[:,i] - self.t[:,j])) for j in self.neighbors[i]]).sum(0) for i in range(self.N)]).flatten()
+        self.e = self._e(self._C, self.t)
 
-        # initialize variables to store proposed values of each
-        self.Cfinal = np.zeros(shape=(self.Dy, self.N*self.Dt))
-        self.tfinal = np.zeros(shape=(self.Dt, self.N))
+        # final means
+        self.C_mean = Cinit
+        self.t_mean = tinit
 
         # cache (x * Sig_x * x)
         self.x_SigX_x = self.x.reshape((1, self.N*self.Dy)).dot(self.sigma_x_inv.dot(self.x.reshape((self.N*self.Dy, 1))))
 
-        self.acceptance = 0
-        
+        # counts
+        self.num_samples, self.accept_rate = 0, 0
+
+        self.Cprop, self.tprop, self.eprop = self.C, self.t, self._e(self._C, self.t)
+
         # initialize variables to store trace and likelihood
-        self.trace, self.likelihoods = [], []
+        self.trace, self.likelihoods = [], [self.likelihood()]
+
+    def _C(self, i):
+        return self.C[:, np.arange(i*self.Dt, (i+1)*self.Dt)]
+
+    def _C_prop(self, i):
+        return self.Cprop[:, np.arange(i*self.Dt, (i+1)*self.Dt)]
+
+    def _e(self, C, t):
+        return np.array([-1 * np.array([self.Vinv.dot(C(i) + C(j)).dot(t[:,i] - t[:,j]) for j in self.neighbors[i]]).sum(0) for i in range(self.N)]).flatten()
 
     def _loglik_x_star(self, e):
         return -0.5*(self.x_SigX_x - 2*self.x.reshape((1, self.N*self.Dy)).dot(e)
                      + e.T.dot(self.sigma_x.dot(e)))[0]
 
     # calculate likelihood for proposed latent variables
-    def likelihood(self, proposed=False):
+    def likelihood(self):
         
-        # x factor changes and y factor added for noisy version **
-        
-        # proposed contains all latent variables in one array
-        if proposed:
-            # calculate likelihood under proposed value
-            Cfactor = matrix_normal_log_star2(self.Cprop, np.zeros(shape=(self.Dy, self.N*self.Dt)), eye(self.Dy), self.C_priorprc)
-            tfactor = matrix_normal_log_star2(self.tprop, np.zeros(self.Dt), eye(self.Dt), self.t_priorprc)
-            #mu_x = np.matrix(self.sigma_x) * self.eprop.reshape((self.N*self.Dy, 1))
-            #xfactor = mvn.logpdf(self.x.reshape((self.N*self.Dy,1)).T, mean=list(mu_x.flat), cov=self.sigma_x, allow_singular=True)
-            xfactor = self._loglik_x_star(self.eprop.reshape((self.N*self.Dy, 1)))
-            #print(Cfactor, tfactor, xfactor)
-            return Cfactor + tfactor + xfactor
-            
-        else:
-            # if proposed is false just calculate it under the current variables
-            Cfactor = matrix_normal_log_star2(self.C, np.zeros(shape=(self.Dy, self.N*self.Dt)), eye(self.Dy), self.C_priorprc)
-            tfactor = matrix_normal_log_star2(self.t, np.zeros(self.Dt), eye(self.Dt), self.t_priorprc)
-            #mu_x = np.matrix(self.sigma_x) * self.e.reshape((self.N*self.Dy, 1))
-            #xfactor = mvn.logpdf(self.x.reshape((self.N*self.Dy, 1)).T, mean=list(mu_x.flat), cov=self.sigma_x, allow_singular=True)
-            xfactor = self._loglik_x_star(self.e.reshape((self.N * self.Dy, 1)))
-            #print(Cfactor, tfactor, xfactor)
-            return Cfactor + tfactor + xfactor
+        C, t, e = self.Cprop, self.tprop, self.eprop
+
+        Cfactor = matrix_normal_log_star2(C, np.zeros(shape=(self.Dy, self.N*self.Dt)),
+                                          eye(self.Dy), self.C_priorprc)
+        tfactor = matrix_normal_log_star2(t, np.zeros(self.Dt), eye(self.Dt), self.t_priorprc)
+        xfactor = self._loglik_x_star(e.reshape((self.N * self.Dy, 1)))
+
+        # print(Cfactor, tfactor, xfactor)
+        return Cfactor + tfactor + xfactor
     
     # update with Metropolis-Hastings step
     def update(self):
-        # calculate likelihoods
-        Lprime = self.likelihood(proposed=True)
-        L = self.likelihood()
+        # calculate likelihood
+        Lprime = self.likelihood()
+        L = self.likelihoods[-1]
         # calculate acceptance probability
         a = 1.0 if Lprime > L else np.exp(Lprime - L)
-        acceptance = bernoulli.rvs(a)
+        accept = bernoulli.rvs(a)
         
         # update the variables
-        if acceptance:
+        if accept:
             self.C = np.copy(self.Cprop)
             self.t = np.copy(self.tprop)
-            self.Ci = [self.C[:,np.arange(i*self.Dt,(i+1)*self.Dt)] for i in range(self.N)]
-            self.e = np.array([-1 * np.array([self.Vinv * np.matrix((self.Ci[i] + self.Ci[j]))*np.matrix((self.t[:,i] - self.t[:,j])) for j in self.neighbors[i]]).sum(0) for i in range(self.N)]).flatten()
-            self.acceptance += 1
-            #add acceptance for x here for noisy version**
-    
-    #propose a new value based on current values
+            self.e = self._e(self._C, self.t)
+            self.likelihoods.append(Lprime)
+
+            # ToDo: add acceptance for x here for noisy version
+
+        else:
+            self.likelihoods.append(L)
+
+        return accept
+
+    # propose a new value based on current values
     def propose(self):
-        # drawn from MVN centered at C
-        self.Cprop = self.C + np.random.randn(self.Dy, self.Dt*self.N)*self.stepsize
-
-        self.Ciprop = [self.Cprop[:, np.arange(i*self.Dt, (i+1)*self.Dt)] for i in range(self.N)]
-
-        #drawn from MVN centered at t
+        self.Cprop = self.C + np.random.randn(self.Dy, self.N*self.Dt)*self.stepsize
         self.tprop = self.t + np.random.randn(self.Dt, self.N)*self.stepsize
+        self.eprop = self._e(self._C_prop, self.tprop)
 
-        self.eprop = np.array([-1 * np.array([self.Vinv * np.matrix((self.Ciprop[i] + self.Ciprop[j]))*np.matrix((self.tprop[:,i] - self.tprop[:,j])) for j in self.neighbors[i]]).sum(0) for i in range(self.N)]).flatten()
-
-        #add proposal for x here for noisy version**
+        # ToDo: add proposal for x here for noisy version
 
     def MH_step(self, burn_in=False):
-        #propose
         self.propose()
-        #update
-        self.update()
-        #store new likelihood
-        self.likelihoods.append(self.likelihood())
+        accept = self.update()
         if not burn_in:
-            self.Cfinal = self.Cfinal + self.C
-            self.tfinal = self.tfinal + self.t
+            self.num_samples += 1
+            self.accept_rate = ((self.num_samples - 1)*self.accept_rate + accept)/self.num_samples
+            self.C_mean = ((self.num_samples - 1)*self.C_mean + self.C)/self.num_samples
+            self.t_mean = ((self.num_samples - 1)*self.t_mean + self.t)/self.num_samples
